e0435780ad487904e62b570670feb8454e674d3e
Adam Houck <adam.houck@crosslead.com>
2020-05-15 10:42:39 -0400

    Fixing fzf install

--- fzf.install.sh
+++ fzf.install.sh
@@ -1,3 +1,2 @@
 #Could also do `brew install fzf` but this opens it up to linux
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.vim/pack/git-pgit-plugins/start/fzf && git clone --depth 1 https://github.com/junegunn/fzf.vim.git ~/.vim/pack/git-plugins/start/fzf.vim
---
+++ nerdtree.install.sh
@@ -0,0 +1,1 @@
+git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree && vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
